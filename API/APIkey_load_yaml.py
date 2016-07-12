0a5d744cf309d451620063b0c5128451b004c0ff
kawasaki-kk <kawasaki.koki@tis.co.jp>
2016-07-12 11:18:32 +0900

    apykey_laod, write, geocode fix

--- API/APIkey_load_yaml.py
+++ API/APIkey_load_yaml.py
@@ -6,7 +6,7 @@ import sys,os
 
 def load_API_KEY(name):
 	try:
-		with open(os.path.dirname(os.path.abspath(__file__))+u"/API_KEY.yaml","r") as f:
+		with open(os.path.dirname(os.path.abspath(__file__))+u"\API_KEY.yaml","r") as f:
 			data = yaml.load(f)[name]["key"]
 			# pprint.pprint(data)
 			return data
--- project_kerotan/kerotan/gmaps_geocoder/gmaps_geocoder.py
+++ project_kerotan/kerotan/gmaps_geocoder/gmaps_geocoder.py
@@ -3,6 +3,7 @@ import googlemaps
 import sys, os
 import traceback
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../../../API/')
 from APIkey_load_yaml import load_API_KEY
 
 import pprint
