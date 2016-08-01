c52325fae6576a5cc1944a3a0c6ddbaf60fda9ef
washinofumihiro <washino.fumihiro@tis.co.jp>
2016-08-01 16:43:04 +0900

    update Gmap_HTML and autocomplete
    ・オートコンプリートの住所と緯度経度をformを経由して値を渡すように設定
    (ただし、forms.pyに新しく記述したせいで、各機能が正常に動作しない)
    ・autocompleteのJSファイルも修正

--- kerotan/forms.py
+++ kerotan/forms.py
@@ -4,6 +4,10 @@ from django import forms
 class AddressForm(forms.Form):
     start_address = forms.CharField(min_length=2, max_length=100, label='start_address')
     arriv_address = forms.CharField(min_length=2, max_length=100, label='arriv_address')
+    start_address2 = forms.CharField(label='start_address2')
+    arriv_address2 = forms.CharField(label='arriv_address2')
+    start_location = forms.CharField(label='start_location')
+    arriv_location = forms.CharField(label='arriv_location')
 
     # latitude = forms.FloatField(label='latitude')
     # longitude = forms.FloatField(label='longitude')
